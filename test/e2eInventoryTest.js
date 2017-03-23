8a012e7ef7c830f6e92ec8c7f14009294e896b28
jaksta <jakub.stachura@agora.pl>
2017-03-23 13:46:20 +0100

    test formatting

--- test/e2eInventoryTest.js
+++ test/e2eInventoryTest.js
@@ -5,7 +5,7 @@ describe('Book inventory', function () {
     it('allows to stock up the items', function (done) {
         request(app)
             .post('/stock')
-            .send({ isbn: '01234567890', count: 10 })
-            .expect({ isbn: '01234567890', count: 10 }, done);
+            .send({isbn: '01234567890', count: 10})
+            .expect({isbn: '01234567890', count: 10}, done);
     })
 });
